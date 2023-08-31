6093d6e3c5e6558dc738d6e89c6df4773b5635cc
Cameron Youngblood <cameron.youngblood@gmail.com>
2023-08-31 11:08:28 -0600

    style: images on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,77 @@
 import Link from "next/link";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Header from "@/components/header";
+
+const images = [
+  {
+    link: "/images/pic1.png",
+    alt: "Landscape Image in the style of Hiroshi Nagai",
+  },
+  {
+    link: "/images/pic2.png",
+    alt: "Landscape Image in the style of Hiroshi Nagai",
+  },
+  {
+    link: "/images/pic3.png",
+    alt: "Landscape Image in the style of Hiroshi Nagai",
+  },
+  {
+    link: "/images/pic4.png",
+    alt: "Landscape Image in the style of Hiroshi Nagai",
+  },
+  {
+    link: "/images/pic5.png",
+    alt: "Landscape Image in the style of Hiroshi Nagai",
+  },
+  {
+    link: "/images/pic6.png",
+    alt: "Landscape Image in the style of Hiroshi Nagai",
+  },
+  {
+    link: "/images/pic7.png",
+    alt: "Landscape Image in the style of Hiroshi Nagai",
+  },
+  {
+    link: "/images/pic8.png",
+    alt: "Landscape Image in the style of Hiroshi Nagai",
+  },
+  {
+    link: "/images/pic9.png",
+    alt: "Landscape Image in the style of Hiroshi Nagai",
+  },
+];
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col container divide-y">
+      <main className="flex min-h-screen flex-col container">
         <section className="mt-6 py-4">
           <div className="space-y-1">
-            <h1 className="text-xl font-bold">NextJS Replicate Template</h1>
-            <p className="text-slate-400">
-              Quickly get your AI application off the ground with replicate and
-              NextJS.
-            </p>
+            <h1 className="text-xl font-bold text-center">
+              Create Lofi Anime-Inspired Landscapes
+            </h1>
           </div>
         </section>
-        <section className="mt-6 py-4">
+        <section className="mt-6 py-4 flex flex-col items-center">
           <div className="space-y-1">
-            <h2 className="font-bold uppercase">How it works</h2>
-            <p className="text-slate-400 max-w-[600px]">
-              This is a template for a simple AI-focused web app that uses a
-              Replicate model, Next-Auth Google authentication, Upstash Rate
-              Limiting, PlanetScale MySQL Database, and ShadcnUI.
-            </p>
+            <h2 className="font-bold text-center">Sign in to start</h2>
           </div>
-          <Button asChild className="mt-6">
+          <Button asChild className="mt-6 w-max">
             <Link href="/create">Try it out</Link>
           </Button>
+        </section>
+        <section className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+          {images.map((image, index) => (
+            <Image
+              key={index}
+              alt={image?.alt}
+              src={image?.link}
+              width={500}
+              height={500}
+            />
+          ))}
         </section>
       </main>
     </>
